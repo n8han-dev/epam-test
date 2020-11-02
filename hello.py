f3576c5caf648bd727c38e64728fcbc457f71a87
n8han-dev <filatov02@gmail.com>
2020-11-02 13:22:44 +0200

    all files added

---
+++ hello.py
@@ -0,0 +1,6 @@
+
+def summ(a, b):
+    return a+b
+
+def mul(a, b):
+    return a*b
---
+++ test_1.py
@@ -0,0 +1,7 @@
+from hello import *
+
+
+def test_success():
+    assert summ(2, 3) == 5
+    assert summ(100, -10) == 90
+
